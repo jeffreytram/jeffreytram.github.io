0d17cd9d796eb5c5fb19f1ecb7e8ab658f15314a
jeffreytram <jeffrey_tram@yahoo.com>
2021-07-16 22:25:39 -0400

    Nav bar and involvement grid now responsive

--- src/components/InvolvementList.js
+++ src/components/InvolvementList.js
@@ -15,21 +15,23 @@ const Involvement = props => {
 
 export default function Involvements() {
   return (
-    <div className={styles.involvementList}>
-      <Involvement
-        title="Solar Forecasting"
-        org="Big Data Big Impact Club"
-        // bannerImage={}
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        link=""
-      />
-      <Involvement
-        title="BuzzBook"
-        org="Student Government Association: IT Board"
-        // bannerImage={}
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        link=""
-      />
+    <div className={styles.involvementListContainer}>
+      <div className={styles.involvementList}>
+        <Involvement
+          title="Solar Forecasting"
+          org="Big Data Big Impact Club"
+          // bannerImage={}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          link=""
+        />
+        <Involvement
+          title="BuzzBook"
+          org="Student Government Association: IT Board"
+          // bannerImage={}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          link=""
+        />
+      </div>
     </div>
   )
 }
